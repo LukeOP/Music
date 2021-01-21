1fb850dc633c7d6b1892c3924b28e8303453e10e
LukeOP <lukenoble@myop.ac.nz>
2021-01-21 16:33:27 +1300

    php

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Music Homepage</title>
+    <link rel="stylesheet" href="styles/styles.css">
+    <link rel="icon" href="favicon.ico">
+</head>
+
+<body>
+    <!-- Main header that is used across all the pages of our website -->
+    <header>
+        <a href="index.php" class="logo"><img id="logo" src="images/MGS_logo.png" alt="Middleton Grange logo"></a>
+        <nav>
+            <ul class="nav_links">
+                <li><a href="Year_9/Year_9.html">Year 9</a></li>
+                <li><a href="Year_10/Year_10.html">Year 10</a></li>
+            </ul>
+        </nav>
+    </header>
+    <!-- Main content -->
+    <main style="max-width: 100%;">
+            <a href="Year_9/Year_9.php"><div class="year-9 content-half" style="height: 600px; background-color: cadetblue;">Year 9</div></a>
+            <a href="Year_10/Year_10.html"><div class="year-10 content-half" style="height: 600px; background-color: coral;">Year 10</div></a>
+        
+
+
+       
+    </main>
+
+</body>
+</html>
