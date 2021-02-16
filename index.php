bc0e3d18779855bf833f7498b039250ef7a185b9
Luke Noble <luke.noble91@hotmail.com>
2021-02-17 12:32:29 +1300

    Commit Test

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Music Homepage</title>
+    <title>Music Homepage - MGS</title>
     <link rel="stylesheet" href="styles/styles.css">
     <link rel="icon" href="/favicon.ico">
 </head>
