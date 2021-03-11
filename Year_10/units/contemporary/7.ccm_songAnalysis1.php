9ccd2d0c684cc497f8a557ae9b6a487de9267272
Luke Noble <luke.noble91@hotmail.com>
2021-03-12 08:15:46 +1300

    Commit

--- Year_10/units/contemporary/7.ccm_songAnalysis1.php
+++ Year_10/units/contemporary/7.ccm_songAnalysis1.php
@@ -32,6 +32,7 @@
         </div>
         <div class="task">
             <h3><img src="/images/task-contemporary.svg" alt="" class="icon">Song 2: Die Free</h3>
+            <div class="image"><img src="/Year_10/images/contemporary/Die_Free.jpg" alt="" class="img-style"></div>
             <p>The lyrics can be found <a href="/downloads/Contemporary_Lyrics/Die Free - Fireflight.pdf" class="text-link" download>here!</a></p>
             <div class="audio"><audio class="audio-style" controls>
                     <source src="/Year_10/audio/Contemporary/01 - Die Free.mp3" type="audio/mp3">
@@ -40,6 +41,7 @@
         </div>
         <div class="task">
             <h3><img src="/images/task-contemporary.svg" alt="" class="icon">Song 3: Fearless</h3>
+            <div class="image"><img src="/Year_10/images/contemporary/josh-garrels-born-again_mnu74eno.jpeg" alt="" class="img-style"></div>
             <p>The lyrics can be found <a href="/downloads/Contemporary_Lyrics/Fearless - Jasmine Murray.pdf" class="text-link" download>here!</a></p>
             <div class="audio"><audio class="audio-style" controls>
                     <source src="/Year_10/audio/Contemporary/01 - Fearless.mp3" type="audio/mp3">
